49a46a91f056efcb6c096298b8ac256bec305684
cafeplays <159272705+cafeplays@users.noreply.github.com>
2026-04-16 16:33:25 +0800

    remove unused dependency

--- Assets/Scripts/SharedProp/parentTargetBone.cs
+++ Assets/Scripts/SharedProp/parentTargetBone.cs
@@ -1,12 +1,10 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using RootMotion.FinalIK;
 
     public class ParentTargetBone : MonoBehaviour
     {
         [SerializeField] public Transform targetTransform;
         [SerializeField] public Vector3 offsets;
         [SerializeField] public Vector3 rotOffsets;
-        public VRIK vrik;
     }
